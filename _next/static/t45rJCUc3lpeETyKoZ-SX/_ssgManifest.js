b427c41e3b4df2b29213cdc27771fa1cb85ed817
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2024-02-03 14:31:36 -0300

    Updates

--- _next/static/t45rJCUc3lpeETyKoZ-SX/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Farts","\u002Farts\u002Fpage\u002F[page]","\u002Fdev","\u002Fprojects\u002F[...slug]","\u002Fultimate-series","\u002Fultimate-series\u002Fpage\u002F[page]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
